3e26d0f26709a12cce7ed43213ef90d64d36bfd8
kasra.hajian <kasra.hajian@divar.ir>
2025-03-22 02:37:32 +0330

    Implement basic rag views using lightrag

---
+++ backend/compliance/service.py
@@ -0,0 +1,110 @@
+import asyncio
+import nest_asyncio
+import os
+
+from lightrag import LightRAG
+from lightrag.kg.shared_storage import initialize_pipeline_status
+from lightrag.llm.llama_index_impl import (
+    llama_index_complete_if_cache,
+    llama_index_embed,
+)
+from lightrag.utils import EmbeddingFunc
+from llama_index.embeddings.litellm import LiteLLMEmbedding
+from llama_index.llms.litellm import LiteLLM
+
+nest_asyncio.apply()
+
+
+GRAPH_RAG_MODES = ["naive", "local", "global", "hybrid"]
+
+
+class GraphRagService:
+    def __init__(self):
+        # Model configuration
+        current_file_dir = os.path.dirname(os.path.abspath(__file__))
+        # TODO: make this configurable so that the chosen directory is a docker volume
+        self.working_dir = os.path.join(current_file_dir, "data", "index_default")
+        self.llm_model = os.environ.get("LLM_MODEL", "gpt-4o")
+        self.embedding_model = os.environ.get(
+            "EMBEDDING_MODEL", "text-embedding-3-large"
+        )
+        self.embedding_max_token_size = int(
+            os.environ.get("EMBEDDING_MAX_TOKEN_SIZE", 8192)
+        )
+        self.litellm_url = os.environ.get("LITELLM_URL", "http://localhost:4000")
+        self.litellm_key = os.environ.get("LITELLM_KEY", "")
+        if not os.path.exists(self.working_dir):
+            os.mkdir(self.working_dir)
+
+    # Initialize LLM function
+    async def llm_model_func(
+        self, prompt, system_prompt=None, history_messages=[], **kwargs
+    ):
+        try:
+            # Initialize LiteLLM if not in kwargs
+            if "llm_instance" not in kwargs:
+                llm_instance = LiteLLM(
+                    model=f"openai/{self.llm_model}",  # Format: "provider/model_name"
+                    api_base=self.litellm_url,
+                    api_key=self.litellm_key,
+                    temperature=0.7,
+                )
+            kwargs["llm_instance"] = llm_instance
+            response = await llama_index_complete_if_cache(
+                kwargs["llm_instance"],
+                prompt,
+                system_prompt=system_prompt,
+                history_messages=history_messages,
+                **kwargs,
+            )
+            return response
+        except Exception as e:
+            print(f"LLM request failed: {str(e)}")
+            raise
+
+    # Initialize embedding function
+    async def embedding_func(self, texts):
+        try:
+            embed_model = LiteLLMEmbedding(
+                model_name=f"openai/{self.embedding_model}",
+                api_base=self.litellm_url,
+                api_key=self.litellm_key,
+            )
+            return await llama_index_embed(texts, embed_model=embed_model)
+        except Exception as e:
+            print(f"Embedding failed: {str(e)}")
+            raise
+
+    # Get embedding dimension
+    async def get_embedding_dim(self):
+        test_text = ["This is a test sentence."]
+        embedding = await self.embedding_func(test_text)
+        embedding_dim = embedding.shape[1]
+        print(f"embedding_dim={embedding_dim}")
+        return embedding_dim
+
+    async def initialize_rag(self):
+        embedding_dimension = await self.get_embedding_dim()
+        rag = LightRAG(
+            working_dir=self.working_dir,
+            llm_model_func=self.llm_model_func,
+            embedding_func=EmbeddingFunc(
+                embedding_dim=embedding_dimension,
+                max_token_size=self.embedding_max_token_size,
+                func=self.embedding_func,
+            ),
+        )
+        await rag.initialize_storages()
+        await initialize_pipeline_status()
+        return rag
+
+
+graph_rag_service = None
+
+
+def get_graph_rag_service():
+    global graph_rag_service
+    if graph_rag_service is None:
+        graph_rag_service = GraphRagService()
+        graph_rag_service = asyncio.run(graph_rag_service.initialize_rag())
+    return graph_rag_service
--- backend/compliance/views.py
+++ backend/compliance/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+import time
 
-# Create your views here.
+from lightrag import QueryParam
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .service import get_graph_rag_service
+from .service import GRAPH_RAG_MODES
+
+
+@api_view(["POST"])
+def insert(request):
+    """
+    API endpoint to insert a document into the GraphRAG service.
+
+    Expects a JSON payload with:
+    {
+        "document": "The document to insert into the GraphRAG service"
+    }
+    """
+    document = request.data.get("document", None)
+    if not document:
+        return Response(
+            {"error": "document is required"}, status=status.HTTP_400_BAD_REQUEST
+        )
+    try:
+        # Get the RAG service
+        rag_service = get_graph_rag_service()
+        # Insert the document into the GraphRAG service
+        rag_service.insert(document)
+        return Response(
+            {"message": "Document inserted successfully"}, status=status.HTTP_200_OK
+        )
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["GET"])
+def query(request):
+    """
+    API endpoint to query the GraphRAG service.
+
+    Expects a JSON payload with:
+    {
+        "query": "The query to ask the GraphRAG service"
+    }
+
+    Returns a JSON response with:
+    {
+        "results": [
+            {
+                "mode": "The mode of the query",
+                "result": "The result of the query",
+                "time": "The time it took to run the query"
+            },
+            ...
+        ]
+    }
+    """
+    query = request.data.get("query", None)
+    if not query:
+        return Response(
+            {"error": "query is required"}, status=status.HTTP_400_BAD_REQUEST
+        )
+    try:
+        # Get the RAG service
+        rag_service = get_graph_rag_service()
+        # Query the rag service with all modes
+        results = []
+        for mode in GRAPH_RAG_MODES:
+            start_time = time.time()
+            result = rag_service.query(query, param=QueryParam(mode=mode))
+            exec_time = time.time() - start_time
+            results.append(
+                {
+                    "mode": mode,
+                    "result": result,
+                    "time": f"{exec_time:.4f}",
+                }
+            )
+        return Response(
+            {
+                "results": results,
+            }
+        )
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
