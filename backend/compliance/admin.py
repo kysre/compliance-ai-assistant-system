ed70fec99e8c083cddbd506e4fa3ec2f5b0dbe24
Kasra Hajian <kasrahajian2002@gmail.com>
2025-04-02 01:45:06 +0330

    Refactor backend models (#7)
    * Refactor & Simplify backend models

--- backend/compliance/admin.py
+++ backend/compliance/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Regulation
+
+
+@admin.register(Regulation)
+class RegulationAdmin(admin.ModelAdmin):
+    list_display = ("identifier", "title", "date", "authority", "link_display")
+    list_filter = ("authority", "date")
+    search_fields = ("identifier", "title", "text")
+    date_hierarchy = "date"
+
+    fieldsets = (
+        (None, {"fields": ("identifier", "title", "date", "authority", "link")}),
+        (_("Content"), {"fields": ("text",), "classes": ("collapse",)}),
+    )
+
+    def link_display(self, obj):
+        if obj.link:
+            return format_html('<a href="{}" target="_blank">View Source</a>', obj.link)
+        return "-"
+
+    link_display.short_description = _("Source Link")
--- backend/compliance/models.py
+++ backend/compliance/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Regulation(models.Model):
+    """
+    Model representing a regulation or law.
+    """
+
+    identifier = models.CharField(max_length=30, help_text=_("Unique Identifier (IDS)"))
+    title = models.CharField(max_length=510, help_text=_("Regulation Title"))
+    date = models.DateField(help_text=_("Approval Date"))
+    authority = models.CharField(max_length=255, help_text=_("Approval Authority"))
+    link = models.URLField(help_text=_("Regulation Link"), blank=True)
+    text = models.TextField(help_text=_("Regulation Text"), blank=True, default="")
+
+    def __str__(self):
+        return f"{self.title} - {self.date}"
+
+    class Meta:
+        verbose_name = _("Regulation")
+        verbose_name_plural = _("Regulations")
+        ordering = ["-date", "title"]
