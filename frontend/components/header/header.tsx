002f30b90b4f0afc9524606c6c3a914b124de3de
kasra <kasrahajian2002@gmail.com>
2025-06-03 04:29:30 +0330

    [frontend] Stick header to the top of the page

--- frontend/components/header/header.tsx
+++ frontend/components/header/header.tsx
@@ -5,7 +5,7 @@ import { ModeToggle } from './mode-toggle';
 
 export const Header = () => {
     return (
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+        <header className="sticky top-0 z-50 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4">
             <SidebarTrigger />
             <Separator orientation="vertical" className="mx-2 h-4" />
             <ModeSelector />
